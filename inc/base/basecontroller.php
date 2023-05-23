c13ed3d19401ce9fb749923cc87a3c661d84f8c6
Levani1111 <papashvililevani@gmail.com>
2023-05-23 17:17:34 +0100

    Code cleanup

--- inc/base/basecontroller.php
+++ inc/base/basecontroller.php
@@ -11,7 +11,7 @@ class basecontroller
     public $plugin_path;
     public $plugin_url;
     public $plugin;
-    // public $managers = array();
+    public $managers = array();
 
     public function __construct()
     {
@@ -19,22 +19,16 @@ class basecontroller
         $this->plugin_url = plugin_dir_url(dirname(__FILE__, 2));
         $this->plugin = plugin_basename(dirname(__FILE__, 3)) . '/leoadd.php';
 
-        // $this->managers = array(
-        //     'cpt_manager' => 'Activate CPT Manager',
-        //     'taxonomy_manager' => 'Activate Taxonomy Manager',
-        //     'media_widget' => 'Activate Media Widget',
-        //     'gallery_manager' => 'Activate Gallery Manager',
-        //     'testimonial_manager' => 'Activate Testimonial Manager',
-        //     'templates_manager' => 'Activate Templates Manager',
-        //     'login_manager' => 'Activate Ajax Login/Signup',
-        //     'membership_manager' => 'Activate Membership Manager',
-        //     'chat_manager' => 'Activate Chat Manager'
-        // );
+        $this->managers = array(
+            'cpt_manager' => 'Activate CPT Manager',
+            'taxonomy_manager' => 'Activate Taxonomy Manager',
+            'media_widget' => 'Activate Media Widget',
+            'gallery_manager' => 'Activate Gallery Manager',
+            'templates_manager' => 'Activate Templates Manager',
+            'testimonial_manager' => 'Activate Testimonial Manager',
+            'login_manager' => 'Activate Ajax Login/Signup',
+            'membership_manager' => 'Activate Membership Manager',
+            'chat_manager' => 'Activate Chat Manager',
+        );
     }
-
-    // public function activated(string $key)
-    // {
-    //     $option = get_option('leoadd_plugin');
-    //     return isset($option[$key]) ? $option[$key] : false;
-    // }
 }
--- inc/pages/admin.php
+++ inc/pages/admin.php
@@ -81,53 +81,15 @@ class admin extends basecontroller
 
     public function set_settings()
     {
-        $args = array(
-            array(
+
+        $args = array();
+        foreach ($this->managers as $key => $value) {
+            $args[] = array(
                 'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'cpt_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'taxonomy_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'media_widget',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'gallery_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'testimonial_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'templates_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'login_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'membership_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-            array(
-                'option_group' => 'leoadd_plugin_settings',
-                'option_name' => 'chat_manager',
-                'callback' => array($this->callbacks, 'checkbox_sanitize')
-            ),
-        );
+                'option_name' => $key,
+                'callback' => array($this->callbacks_manager, 'checkbox_sanitize')
+            );
+        }
 
         $this->settings->set_settings($args);
     }
@@ -148,107 +110,20 @@ class admin extends basecontroller
 
     public function set_fields()
     {
-        $args = array(
-            array(
-                'id' => 'cpt_manager',
-                'title' => 'Activate CPT Manager',
+        $args = array();
+        foreach ($this->managers as $key => $value) {
+            $args[] = array(
+                'id' => $key,
+                'title' => $value,
                 'callback' => array($this->callbacks_manager, 'checkbox_field'),
                 'page' => 'leoadd_plugin',
                 'section' => 'leoadd_admin_index',
                 'args' => array(
-                    'label_for' => 'cpt_manager',
+                    'label_for' => $key,
                     'class' => 'ui-toggle'
                 )
-            ),
-            array(
-                'id' => 'taxonomy_manager',
-                'title' => 'Activate Taxonomy Manager',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'taxonomy_manager',
-                    'class' => 'ui-toggle'
-                )
-            ),
-            array(
-                'id' => 'media_widget',
-                'title' => 'Activate Media Widget',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'media_widget',
-                    'class' => 'ui-toggle'
-                )
-            ),
-            array(
-                'id' => 'gallery_manager',
-                'title' => 'Activate Gallery Manager',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'gallery_manager',
-                    'class' => 'ui-toggle'
-                )
-            ),
-            array(
-                'id' => 'testimonial_manager',
-                'title' => 'Activate Testimonial Manager',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'testimonial_manager',
-                    'class' => 'ui-toggle'
-                )
-            ),
-            array(
-                'id' => 'templates_manager',
-                'title' => 'Activate Templates Manager',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'templates_manager',
-                    'class' => 'ui-toggle'
-                )
-            ),
-            array(
-                'id' => 'login_manager',
-                'title' => 'Activate Ajax Login/Signup',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'login_manager',
-                    'class' => 'ui-toggle'
-                )
-            ),
-            array(
-                'id' => 'membership_manager',
-                'title' => 'Activate Membership Manager',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'membership_manager',
-                    'class' => 'ui-toggle'
-                )
-            ),
-            array(
-                'id' => 'chat_manager',
-                'title' => 'Activate Chat Manager',
-                'callback' => array($this->callbacks_manager, 'checkbox_field'),
-                'page' => 'leoadd_plugin',
-                'section' => 'leoadd_admin_index',
-                'args' => array(
-                    'label_for' => 'chat_manager',
-                    'class' => 'ui-toggle'
-                )
-            ),
-        );
+            );
+        }
 
         $this->settings->set_fields($args);
     }
