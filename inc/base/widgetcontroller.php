23dba6b2e1f0e31b7a5c8f31e5022c87637db3ab
Levani1111 <papashvililevani@gmail.com>
2023-07-02 17:59:11 +0100

    Custom Widget

--- inc/base/widgetcontroller.php
+++ inc/base/widgetcontroller.php
@@ -6,9 +6,9 @@
 
 namespace Inc\base;
 
-use \Inc\Api\settingsapi;
+
 use \Inc\base\basecontroller;
-use \Inc\Api\callbacks\admincallbacks;
+use \Inc\Api\Widgets\mediawidget;
 
 /**
 * 
@@ -16,36 +16,14 @@ use \Inc\Api\callbacks\admincallbacks;
 class widgetcontroller extends basecontroller
 {
 
-    public $settings;
-    public $callbacks;
-
-    public $subpages = array();
 
 	public function register()
 	{
 		if ( ! $this->activated( 'media_widget' ) ) return;
 
-		$this->settings = new settingsapi();
-
-        $this->callbacks = new admincallbacks();
-
-        $this->set_subpages();
-
-        $this->settings->add_sub_pages($this->subpages)->register();
-
+		
+        $media_widget = new mediawidget();
+		$media_widget->register();
+        
 	}
-
-	public function set_subpages()
-    {
-        $this->subpages = array(
-            array(
-                'parent_slug' => 'leoadd_plugin',
-                'page_title' => 'Widgets Manager', 
-				'menu_title' => 'Widgets Manager', 
-				'capability' => 'manage_options', 
-                'menu_slug' => 'leoadd_widget',
-                'callback' => array($this->callbacks, 'admin_widgets'),
-            )
-        );
-    }
 }
