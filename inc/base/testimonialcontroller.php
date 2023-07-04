63b9c6a79b03815a935df04bf205809cc6aaeaa6
Levani1111 <papashvililevani@gmail.com>
2023-07-04 11:49:22 +0100

    Testimonial Manager

--- inc/base/testimonialcontroller.php
+++ inc/base/testimonialcontroller.php
@@ -16,7 +16,7 @@ use \Inc\Api\callbacks\admincallbacks;
 class testimonialcontroller extends basecontroller
 {
 
-    public $settings;
+    
     public $callbacks;
 
     public $subpages = array();
@@ -28,6 +28,9 @@ class testimonialcontroller extends basecontroller
         add_action('init', array($this, 'testimonial_cpt'));
         add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
         add_action('save_post', array($this, 'save_meta_box'));
+        add_action( 'manage_testimonial_posts_columns', array( $this, 'set_custom_columns' ) );
+		add_action( 'manage_testimonial_posts_custom_column', array( $this, 'set_custom_columns_data' ), 10, 2 );
+        add_filter( 'manage_edit-testimonial_sortable_columns', array( $this, 'set_custom_columns_sortable' ) );
     }
 
     public function testimonial_cpt()
@@ -54,49 +57,124 @@ class testimonialcontroller extends basecontroller
         add_meta_box(
             'testimonial_author',
             'Author',
-            array($this, 'render_author_box'),
+            array($this, 'render_features_box'),
             'testimonial',
             'side',
             'default'
         );
-
-        // author email
-        // approved [checkbox]
-        // featured [checkbox]
     }
 
-    public function render_author_box($post)
-    {
-        wp_nonce_field('leoadd_testimonial_author', 'leoadd_testimonial_author_nonce');
+    public function render_features_box($post)
+	{
+		wp_nonce_field( 'leoadd_testimonial', 'leoadd_testimonial_nonce' );
 
-        $value = get_post_meta($post->ID, '_leoadd_testimonial_author_key', true);
-        ?>
-            <label for="leoadd_testimonial_author">Testimonial Author</label>
-            <input type="text" id="leoadd_testimonial_author" name="leoadd_testimonial_author" value="<?php echo esc_attr($value); ?>">
-        <?php
-    }
+		$data = get_post_meta( $post->ID, '_leoadd_testimonial_key', true );
+		$name = isset($data['name']) ? $data['name'] : '';
+		$email = isset($data['email']) ? $data['email'] : '';
+		$approved = isset($data['approved']) ? $data['approved'] : false;
+		$featured = isset($data['featured']) ? $data['featured'] : false;
+		?>
+            <p>
+                <label class="meta-label" for="leoadd_testimonial_author">Author Name</label>
+                <input type="text" id="leoadd_testimonial_author" name="leoadd_testimonial_author" class="widefat" value="<?php echo esc_attr( $name ); ?>">
+            </p>
+            <p>
+                <label class="meta-label" for="leoadd_testimonial_email">Author Email</label>
+                <input type="email" id="leoadd_testimonial_email" name="leoadd_testimonial_email" class="widefat" value="<?php echo esc_attr( $email ); ?>">
+            </p>
+            <div class="meta-container">
+                <label class="meta-label w-50 text-left" for="leoadd_testimonial_approved">Approved</label>
+                <div class="text-right w-50 inline">
+                    <div class="ui-toggle inline"><input type="checkbox" id="leoadd_testimonial_approved" name="leoadd_testimonial_approved" value="1" <?php echo $approved ? 'checked' : ''; ?>>
+                        <label for="leoadd_testimonial_approved"><div></div></label>
+                    </div>
+                </div>
+            </div>
+            <div class="meta-container">
+                <label class="meta-label w-50 text-left" for="leoadd_testimonial_featured">Featured</label>
+                <div class="text-right w-50 inline">
+                    <div class="ui-toggle inline"><input type="checkbox" id="leoadd_testimonial_featured" name="leoadd_testimonial_featured" value="1" <?php echo $featured ? 'checked' : ''; ?>>
+                        <label for="leoadd_testimonial_featured"><div></div></label>
+                    </div>
+                </div>
+            </div>
+		<?php
+	}
 
 
     public function save_meta_box($post_id)
-    {
-        if (!isset($_POST['leoadd_testimonial_author_nonce'])) {
-            return $post_id;
-        }
+	{
+		if (! isset($_POST['leoadd_testimonial_nonce'])) {
+			return $post_id;
+		}
 
-        $nonce = $_POST['leoadd_testimonial_author_nonce'];
-        if (!wp_verify_nonce($nonce, 'leoadd_testimonial_author')) {
-            return $post_id;
-        }
+		$nonce = $_POST['leoadd_testimonial_nonce'];
+		if (! wp_verify_nonce( $nonce, 'leoadd_testimonial' )) {
+			return $post_id;
+		}
 
-        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-            return $post_id;
-        }
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return $post_id;
+		}
 
-        if (!current_user_can('edit_post', $post_id)) {
-            return $post_id;
-        }
+		if (! current_user_can( 'edit_post', $post_id ) ) {
+			return $post_id;
+		}
 
-        $data = sanitize_text_field($_POST['leoadd_testimonial_author']);
-        update_post_meta($post_id, '_leoadd_testimonial_author_key', $data);
-    }
+		$data = array(
+			'name' => sanitize_text_field( $_POST['leoadd_testimonial_author'] ),
+			'email' => sanitize_text_field( $_POST['leoadd_testimonial_email'] ),
+			'approved' => isset($_POST['leoadd_testimonial_approved']) ? 1 : 0,
+			'featured' => isset($_POST['leoadd_testimonial_featured']) ? 1 : 0,
+		);
+		update_post_meta( $post_id, '_leoadd_testimonial_key', $data );
+	}
+
+    public function set_custom_columns($columns)
+	{
+		$title = $columns['title'];
+		$date = $columns['date'];
+		unset( $columns['title'], $columns['date'] );
+
+		$columns['name'] = 'Author Name';
+		$columns['title'] = $title;
+		$columns['approved'] = 'Approved';
+		$columns['featured'] = 'Featured';
+		$columns['date'] = $date;
+
+		return $columns;
+	}
+
+
+	public function set_custom_columns_data($column, $post_id)
+	{
+		$data = get_post_meta( $post_id, '_leoadd_testimonial_key', true );
+		$name = isset($data['name']) ? $data['name'] : '';
+		$email = isset($data['email']) ? $data['email'] : '';
+		$approved = isset($data['approved']) && $data['approved'] === 1 ? '<strong>YES</strong>' : 'NO';
+		$featured = isset($data['featured']) && $data['featured'] === 1 ? '<strong>YES</strong>' : 'NO';
+
+		switch($column) {
+			case 'name':
+				echo '<strong>' . $name . '</strong><br/><a href="mailto:' . $email . '">' . $email . '</a>';
+				break;
+
+			case 'approved':
+				echo $approved;
+				break;
+
+			case 'featured':
+				echo $featured;
+				break;
+		}
+	}
+
+	public function set_custom_columns_sortable($columns)
+	{
+		$columns['name'] = 'name';
+		$columns['approved'] = 'approved';
+		$columns['featured'] = 'featured';
+
+		return $columns;
+	}
 }
