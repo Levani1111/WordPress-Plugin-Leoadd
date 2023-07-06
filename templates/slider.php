5fefeab178930470f25f5252b4ea3d63453dfd07
Levani1111 <papashvililevani@gmail.com>
2023-07-06 15:35:48 +0100

    Testimonial Slider

--- templates/slider.php
+++ templates/slider.php
@@ -13,14 +13,23 @@ $args = array(
     )
 );
 
-$query = new WP_Query( $args );
+$query = new WP_Query($args);
 
 if ($query->have_posts()) :
-    echo '<ul>';
+    $i = 1;
+
+    echo '<div class="ac-slider--wrapper"><div class="ac-slider--container"><div class="ac-slider--view"><ul>';
+
     while ($query->have_posts()) : $query->the_post();
-        echo '<li>'.get_the_title().'<p>'.get_the_content().'</p></li>';
+        $name = get_post_meta(get_the_ID(), '_leoadd_testimonial_key', true)['name'] ?? '';
+
+        echo '<li class="ac-slider--view__slides' . ($i === 1 ? ' is-active' : '') . '"><p class="testimonial-quote">"' . get_the_content() . '"</p><p class="testimonial-author">~ ' . $name . ' ~</p></li>';
+
+        $i++;
     endwhile;
-    echo '</ul>';
+
+    echo '</ul></div><div class="ac-slider--arrows"><span class="arrow ac-slider--arrows__left">&#x3c;</span><span class="arrow ac-slider--arrows__right">&#x3e;</span></div></div></div>';
+
 endif;
 
 wp_reset_postdata();
