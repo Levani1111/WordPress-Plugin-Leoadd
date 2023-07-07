2308ebec2e7acdc205b7e67beb0ce628272a6478
Levani1111 <papashvililevani@gmail.com>
2023-07-07 15:56:23 +0100

    Ajax Login Form part 2

--- inc/base/authcontroller.php
+++ inc/base/authcontroller.php
@@ -20,6 +20,7 @@ class authcontroller extends basecontroller
 
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
 		add_action( 'wp_head', array( $this, 'add_auth_template' ) );
+        add_action( 'wp_ajax_nopriv_leoadd_login', array( $this, 'login' ) );
 
 	}
 
@@ -27,7 +28,7 @@ class authcontroller extends basecontroller
 	{
 
         if ( is_user_logged_in() ) return;
-        
+
 		wp_enqueue_style( 'authstyle', $this->plugin_url . 'assets/auth.css' );
 		wp_enqueue_script( 'authscript', $this->plugin_url . 'assets/auth.js' );
 	}
@@ -41,5 +42,37 @@ class authcontroller extends basecontroller
 		if ( file_exists( $file ) ) {
 			load_template( $file, true );
 		}
+	}
+
+    public function login()
+	{
+		check_ajax_referer( 'ajax-login-nonce', 'leoadd_auth');
+
+		$info = array();
+		$info['user_login'] = $_POST['username'];
+		$info['user_password'] = $_POST['password'];
+		$info['remember'] =true;
+
+		$user_signon = wp_signon( $info, true );
+
+		if ( is_wp_error( $user_signon) ) {
+			echo json_encode(
+				array(
+					'status' => false,
+					'message' => 'Wrong username or password'
+				)
+			);
+
+			die();
+		}
+
+		echo json_encode(
+			array(
+				'status' => true,
+				'message' => 'Login successful, redirecting...'
+			)
+		);
+
+		die();
 	}
 }
